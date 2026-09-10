623f75350c5c2a18f0354ad9c61dc3589c94d208
adh01640-beep <adh01640@gmail.com>
2026-09-11 00:29:45 +0300

    Update plugin author name in build.gradle.kts

--- plugins/AutocompleteFix/build.gradle.kts
+++ plugins/AutocompleteFix/build.gradle.kts
@@ -8,6 +8,6 @@ aliucord {
         * Initial plugin release.
         """.trimIndent()
     )
-    author("lavadesu", 368398754077868032L, hyperlink = true)
+    author("Adham", 368398754077868032L, hyperlink = true)
     deploy.set(true)
 }
