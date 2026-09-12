23df3c0b5871ac347ecb52b676d380bca07de762
adh01640-beep <adh01640@gmail.com>
2026-09-12 03:34:13 +0300

    Initialize SmartProfileEditor plugin with version 1.0.0

--- plugins/SmartProfileEditor/build.gradle.kts
+++ plugins/SmartProfileEditor/build.gradle.kts
@@ -1,1 +1,13 @@
+version = "1.0.0"
+description = "edit your profile with new options: profile effect, server tag, and more!"
 
+aliucord {
+    changelog.set(
+        """
+        # 1.0.0
+        * Initial plugin release.
+        """.trimIndent()
+    )
+    author("Adham")
+    deploy.set(true)
+}
