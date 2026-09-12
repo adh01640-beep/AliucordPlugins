f701a60abd28eab185a6bdd30a1231057363ea19
adh01640-beep <adh01640@gmail.com>
2026-09-12 21:09:19 +0300

    Add CheckLinks plugin with initial configuration
    Initial release of the CheckLinks plugin with version 1.0.0.

---
+++ plugins/CheckLinks/build.gradle.kts
@@ -0,0 +1,13 @@
+version = "1.0.0"
+description = "check links before opening them."
+
+aliucord {
+    changelog.set(
+        """
+        # 1.0.0
+        * Initial plugin release.
+        """.trimIndent()
+    )
+    author("Adham")
+    deploy.set(true)
+}
