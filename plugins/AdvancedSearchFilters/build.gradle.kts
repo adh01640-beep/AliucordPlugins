2321e81745012f4d43dfa60ef2a9fb7fc098b635
adh01640-beep <adh01640@gmail.com>
2026-09-12 17:20:55 +0300

    Update plugin description for AdvancedSearchFilters

--- plugins/AdvancedSearchFilters/build.gradle.kts
+++ plugins/AdvancedSearchFilters/build.gradle.kts
@@ -2,7 +2,7 @@ import com.aliucord.gradle.AliucordExtension
 
 version = "1.0.0"
 
-description = "Shows every server role in the @mention autocomplete list, even roles you don't have permission to ping. Each role is labeled (mention) or (silent) depending on whether selecting it will actually notify that role. Toggleable in the plugin settings."
+description = "Shows all Discord search filters in Aliucord."
 
 configure<AliucordExtension> {
     // TODO: replace 0L with your real Discord user ID if you want the
