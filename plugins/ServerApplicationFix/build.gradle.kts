d2ab29cd512672a1e5e495890786e024ba6d8243
adh01640-beep <adh01640@gmail.com>
2026-09-13 19:29:45 +0300

    Add initial server application fix plugin
    Initial release of the server application fix plugin with versioning and changelog.

---
+++ plugins/ServerApplicationFix/build.gradle.kts
@@ -0,0 +1,13 @@
+version = "1.0.0"
+description = "Fixes server applications."
+
+aliucord {
+    changelog.set(
+        """
+        # 1.0.0
+        * Initial plugin release.
+        """.trimIndent()
+    )
+    author("Adham", 1001222848716738570L, hyperlink = true)
+    deploy.set(true)
+}
