7900f0106e50cad38915edeb6a35fe8cbb727f0c
adh01640-beep <adh01640@gmail.com>
2026-09-16 18:21:25 +0300

    Add build.gradle.kts for CloneSender plugin
    Initial release of the CloneSender plugin with version 1.0.0.

---
+++ plugins/CloneSender/build.gradle.kts
@@ -0,0 +1,13 @@
+version = "1.0.0"
+description = "-رسالة"
+
+aliucord {
+    changelog.set(
+        """
+        # 1.0.0
+        * Initial plugin release.
+        """.trimIndent()
+    )
+    author("Adham")
+    deploy.set(true)
+}
