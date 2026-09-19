f0219643bb0121b46933f784752a2154433b24ab
adhamgithub <adh01640@gmail.com>
2026-09-19 20:08:08 +0300

    Add build.gradle.kts for InstantFinishQuests plugin
    Initial release of the InstantFinishQuests plugin with versioning and changelog.

---
+++ plugins/InstantFinishQuests/build.gradle.kts
@@ -0,0 +1,13 @@
+version = "1.0.0"
+description = "Seamlessly completes available Discord quests in the background and provides an integrated menu to track and claim your earned rewards."
+
+aliucord {
+    changelog.set(
+        """
+        # 1.0.0
+        * Initial plugin release.
+        """.trimIndent()
+    )
+    author("Adham", 1001222848716738570L, hyperlink = true)
+    deploy.set(true)
+}
